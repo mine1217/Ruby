be02366b9ac9575205790dfe152be3fdbaa80253
Minoru Makino <makino111217@gmail.com>
2021-03-14 08:59:50 +0000

    [ADD]シングルクォートとダブルの違いについて確認

--- sample/2-5/weitght.rb
+++ sample/2-5/weitght.rb
@@ -2,4 +2,5 @@ name = "A"
 weight = 50
 
 puts name + "さんの体重は" + weight.to_s + "kgです"
-puts "#{name}さんの体重は#{weight}kgです" #フォーマットみたいな　文字列に統一できる　変数展開
+puts "#{name}さんの体重は#{weight}kgです" #フォーマットみたいな　文字列に統一できる　変数展開っていうらしい
+puts '#{name}さんの体重は#{weight}kgです' #シングルクォートだと強制的にそのまま文字列にされる jsと同じ

4211177f2975b87b054ad704306f7855fc5ea577
Minoru Makino <makino111217@gmail.com>
2021-03-16 03:47:17 +0000

    [ADD]elsifの確認

---
+++ sample/2-6/apple.rb
@@ -0,0 +1,12 @@
+NAGANO = "Nagano"
+AOMORi = "Aomori"
+
+apple = NAGANO
+
+if apple == AOMORI
+  puts "このリンゴは青森産です"
+elsif apple == NAGANO
+  puts "このリンゴは青森産ではなく、長野県産です"
+else
+    puts "このリンゴは青森県産でも長野県産でもありません。"
+end
